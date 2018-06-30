59f1f61021c8a1873db4491b643ab28158186b07
Malak Tayeh <malaktayeh22@gmail.com>
2018-06-30 08:55:52 -0400

    Setting the Enemy initial location

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +14,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    ctx.drawImage(Resources.get(this.sprite), 0, 0);
 };
 
 // Draw the enemy on the screen, required method for game
